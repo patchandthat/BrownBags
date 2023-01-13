16b88fee76df093b64face47f498de51c042e8d7
Patrick Allwood <Patrick.Allwood@rocksolidknowledge.com>
2023-01-13 12:39:28 +0000

    New up parser

--- AntlrDemo/AntlrDemo.Code/StringCalculator.cs
+++ AntlrDemo/AntlrDemo.Code/StringCalculator.cs
@@ -1,10 +1,18 @@
+using Antlr4.Runtime;
+using Parser;
+
 namespace AntlrDemo.Code
 {
     public class StringCalculator
     {
         public string Evaluate(string expression)
         {
-            throw new NotImplementedException();
+            var charStream = CharStreams.fromString(expression);
+            var lexer = new CalculatorLexer(charStream);
+            var calculatorTokens = new CommonTokenStream(lexer);
+            var parser = new CalculatorParser(calculatorTokens);
+
+            return "";
         }
     }
 }
