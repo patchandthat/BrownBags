e86d05f3a556522c697c1dbe393225e1b1697f62
Patrick Allwood <Patrick.Allwood@rocksolidknowledge.com>
2023-01-13 12:42:03 +0000

    Parse the body rule

--- AntlrDemo/AntlrDemo.Code/StringCalculator.cs
+++ AntlrDemo/AntlrDemo.Code/StringCalculator.cs
@@ -1,4 +1,5 @@
 using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
 using Parser;
 
 namespace AntlrDemo.Code
@@ -11,6 +12,9 @@ namespace AntlrDemo.Code
             var lexer = new CalculatorLexer(charStream);
             var calculatorTokens = new CommonTokenStream(lexer);
             var parser = new CalculatorParser(calculatorTokens);
+
+            // Will go bang if the input syntax is incorrect
+            IParseTree expressionTree = parser.body();
 
             return "";
         }
